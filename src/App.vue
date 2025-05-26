7fad2847eed8e4bd139669decae266418961c63a
Christian <b.christian.fuentes@gmail.com>
2025-05-26 19:37:27 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Formulario />
+  </main>
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Formulario
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Formulario',
+  props:[],
+  data() {
+    return {
+      // Define your form data here
+      textoIngresado: '',
+    }
+  },
+  methods: {
+    // Define your form methods here
+  },
+  computed: {
+    // Define your computed properties here
+  },
+}
+</script>
+
+<style scoped>
+</style>
